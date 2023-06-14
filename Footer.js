caceef5ad0b9af2b0da4c16326db40d3af7e32ec
Ferinco <emmanuelferrum003@gmail.com>
2023-06-14 11:28:10 +0100

    site responsiveness

---
+++ Footer.js
@@ -0,0 +1,26 @@
+export default function Footer (){
+    return(
+        <div className="footer">
+<div className="footer-top">
+    <h3>FerinSTORE</h3>
+    <ul>
+        <li><a href=""><img src="./Images/Social/social (1).png"/></a></li>
+        <li><a href=""><img src="./Images/Social/social (2).png"/></a></li>
+        <li><a href=""><img src="./Images/Social/social (3).png"/></a></li>
+        <li><a href=""><img src="./Images/Social/social (4).png"/></a></li>
+
+    </ul>
+</div>
+<div className="footer-bottom">
+    <p>copyright.....</p>
+    <ul>
+        <li><a href="">Terms of Service</a></li>
+        <li><a href="">Policy Service</a></li>
+        <li><a href="">Cookie Policy</a></li>
+        <li><a href="">Partners</a></li>
+
+    </ul>
+</div>
+        </div>
+    )
+}
---
+++ Right.js
@@ -0,0 +1,19 @@
+export default function Right(){
+    return(
+        <div className="right">
+<div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="../Images/sidebar2.avif" alt="First slide"/>
+    </div>
+    <div className="carousel-item">
+      <img src="../Images/sidebar4.avif" alt="Second slide"/>
+    </div>
+    <div className="carousel-item">
+      <img src="../Images/sidebar5.avif" alt="Third slide"/>
+    </div>
+  </div>
+</div>
+        </div>
+    )
+}
