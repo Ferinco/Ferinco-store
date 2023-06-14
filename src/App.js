33cc695b2a2bb2089ffeb8d897d5aec25da0f807
Ferinco <emmanuelferrum003@gmail.com>
2023-06-14 13:13:00 +0100

    navbar redesign

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Banner from "./Components/Banner";
+import Products from "./Components/Product";
+import Collections from "./Components/Collections";
+import Styles from "./Components/Styles";
+import Footer from "./Components/Footer";
+function App() {
+  return (
+    <div className="App">
+<Banner/>
+<Products/>
+<Collections/>
+<Styles/>
+<Footer/>
+    </div>
+  );
+}
+
+export default App;
