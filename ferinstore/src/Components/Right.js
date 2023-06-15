68f52a1f4371555c4f7de31752f8d7acdf9bdeea
Ferinco <emmanuelferrum003@gmail.com>
2023-06-15 18:51:51 +0100

    fonts and frameworks

--- ferinstore/src/Components/Right.js
+++ ferinstore/src/Components/Right.js
@@ -1,3 +1,4 @@
+import React from 'react';
 export default function Right(){
     return(
         <div className="right">
--- ferinstore/src/index.js
+++ ferinstore/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './Style.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
