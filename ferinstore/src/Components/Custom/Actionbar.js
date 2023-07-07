d4be7e69c660d43d5d41c280eaccc50816ae4224
Ferinco <emmanuelferrum003@gmail.com>
2023-07-07 21:06:00 +0100

    new navbar designs

---
+++ ferinstore/src/Components/Custom/Actionbar.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+export default function ActionBar() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+const ActionBar = styled.div `
+background-color: rgb(143, 241, 143);
+border-radius: 10px;
+width: 100% !important;
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 10px 20px;
+justify-content: space-between !important;`
--- ferinstore/src/Components/Navbar.js
+++ ferinstore/src/Components/Navbar.js
@@ -1,32 +1,104 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import styled from "styled-components"
 export default function Navbar(){
     return(
-        
-            <nav className="navbar-expand-lg">
-  <Link className="navbar-brand" to="/">FerinSTORE</Link>
-  <div>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <Link className="nav-link" to="/details">Checkout</Link>
-      </li>
-      <li className="nav-item">
+<NAV>
+<input type="checkbox" id="check"/>
+<label for="check" className="checkbtn">
+  <i className="fas fa-bars"></i>
+</label>
+<Link className="navbar-brand" to="/">FerinSTORE</Link>
+<ul className="navbar-nav">
+  <li className="nav-item active">
+      <Link className="nav-link" to="/details">Checkout</Link>
+       </li>
+       <li className="nav-item">
         <a className="nav-link" href="#">Collections</a>
-      </li>
-      <li className="nav-item">
+       </li>
+       <li className="nav-item">
         <a className="nav-link" href="#">Blog</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">FAQs</a>
-      </li>
-    </ul>
-  </div>
-  </div>
-</nav>
-       
+     </li>
+     <li className="nav-item"> 
+     <a className="nav-link" href="#">FAQs</a>
+       </li>
+</ul>
+</NAV>
     )
 }
+const NAV = styled.div `
+margin: 0 !important;
+display: flex;
+flex-direction: row;
+  background: rgb(143, 241, 143);
+ align-items: center;
+  padding: 10px 20px;
+  border-radius: 10px;
+width: 100% !important;
+ justify-content: space-between !important;
+ .navbar-nav{
+    gap: 10px;
+    display: flex;
+    flex-direction: row;
+    a{
+      padding-bottom: 5px;
+      &:hover{
+         padding-bottom: 5px;
+         border-bottom:  1px solid rgb(243, 255, 243);
+       }
+    }
+  }
+.checkbtn{
+  font-size: 30px;
+  color: white;
+  float: right;
+  line-height: 80px;
+  margin-right: 40px;
+  cursor: pointer;
+  display: none;
+}
+#check{
+  display: none;
+}
+@media (max-width: 952px){
+
+  nav ul li a{
+    font-size: 16px;
+  }
+}
+@media (max-width: 858px){
+  .checkbtn{
+    display: block;
+  }
+  ul{
+    position: fixed;
+    width: 100%;
+    height: 100vh;
+    background: #2c3e50;
+    top: 80px;
+    left: -100%;
+    text-align: center;
+    transition: all .5s;
+  }
+ NAV ul li{
+    display: block;
+    margin: 50px 0;
+    line-height: 30px;
+  }
+ NAV ul li a{
+    font-size: 20px;
+  }
+  a:hover,a.active{
+    background: none;
+    color: #0082e6;
+  }
+  #check:checked ~ ul{
+    left: 0;
+  }
+}
+section{
+  background: url(bg1.jpg) no-repeat;
+  background-size: cover;
+  height: calc(100vh - 80px);
+}
+`
